0592df860bc24c9fe6a43df3cd5e6ac17952e893
svpontes <pontes_sergio@hotmail.com>
2026-03-12 21:26:41 +0000

    send initial code

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers;
+
+public class  HelloWorldController : Controller
+{
+    //GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    //GET: /HeloWorld/Welcome/
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
